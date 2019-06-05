2015c5a9e76cdccffbe1ceed0eac809240b62e4d
JJ Garzella <jjgarzella@gmail.com>
2019-06-05 09:03:41 -0400

    remove pell class stuff from experiments

--- Experiments.jl
+++ Experiments.jl
@@ -2,10 +2,12 @@ module Experiments
 
 include("./PartitionsGen.jl")
 include("./QuadraticPartitions.jl")
+include("./PellClasses.jl")
 #include("./EulerCoefficients.jl")
 
 using .PartitionsGen
 using .QuadraticPartitions
+using .PellClasses
 #using .EulerCoefficients
 using SymPy
 using AbstractAlgebra
@@ -154,18 +156,6 @@ function process_new_coefficients(filename,D,allpositive)
   QuadraticPartitions.incorporate_coefficients(coefs,D,allpositive)
 end
 
-function generate_norm_array(N,D)
-  M = ceil(Int, N/√D)
-  NORMS = zeros(Int, N+1,M+1)
-  for a = 0:N
-    for b = 0:M
-      if Experiments.QuadraticPartitions.is_wholly_positive(a,b,D)
-        NORMS[a+1,b+1] = a^2 - D*b^2
-      end
-    end
-  end
-  NORMS
-end
 
 function nonzerocoefs(M,level=-10000)
   A = deepcopy(M)
@@ -230,57 +220,6 @@ function highestBFor(a,D)
   floor(Int, a / √D)
 end
 
-# goes one member beyond limitA right now
-function pellClassFor(a,b,D=2,unit=(3,2),limitA=100)
-  class = [(a,b)]#Array{Tuple{typeof(a),typeof(b)}}(undef,0)
-  current = (a,b)
-  while current[1] <= limitA
-    current = quadratic_mult(current,unit,D)
-    class = [class; current]
-  end
-
-  conjunit = (unit[1], -unit[2])
-  current = (a,b)
-  while current[1] <= limitA
-    current = quadratic_mult(current,conjunit,D)
-    class = [class; current]
-  end
-  class
-end
-
-samepellclass(cl1,cl2) = (Set(cl1) == Set(cl2))
-
-function addconjugates(pellclass)
-  class = Set(pellclass)
-  for (a,b) in pellclass
-    push!(class,(a,-b))
-  end
-  collect(class)
-end
-
-const PellClass{T} = Array{Tuple{T,T}}
-
-function findpellclasses(N,D=2,unit=(3,2))
-  norms = generate_norm_array(N,D)
-  classes = Dict{Int,Array{PellClass{Int}}}()
-  boundOnFundSol = floor(Int,N/√(unit[1] + unit[2]*√D))
-
-  pushclass(c,i) = haskey(classes,i) ? push!(classes[i], c) : classes[i] = [c]
-
-  for i = 1:boundOnFundSol
-    matches = [(ind[1],ind[2]) for ind in findall(norms .== i)]
-    matches = map(m -> m .- 1, matches)
-
-    while 0 < length(matches)
-      firstclass = pellClassFor(matches[1]...,D,unit,N)
-      pushclass(firstclass, i)
-
-      accumSet = addconjugates(firstclass)
-      matches = filter(m -> !(m in accumSet), matches)
-    end
-  end
-  classes
-end
 
 function comparepellclasses(N,D=2,unit=(3,2),allclasses=nothing)
   allclasses == nothing && (allclasses = Experiments.findpellclasses(N))
@@ -301,15 +240,6 @@ function comparepellclasses(N,D=2,unit=(3,2),allclasses=nothing)
   outliers
 end
 
-function pellclasses_to_thetafn(pellclasses)
-  θ = zeros(Int, maximum(keys(pellclasses)))
-  for (norm,classes) in pellclasses
-    θ[norm] = length(classes)
-  end
-  θ
-end
     
-    
-
 
 end#module
