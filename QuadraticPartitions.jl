22cd32c5d7332d3237f3dd1e5d5d9986a54e3512
JJ Garzella <jjgarzella@gmail.com>
2019-06-24 15:12:46 -0400

    Add fast algorithm

--- QuadraticPartitions.jl
+++ QuadraticPartitions.jl
@@ -1,11 +1,14 @@
 module QuadraticPartitions
 
 include("./EulerCoefficients.jl")
+include("./TotallyLess.jl")
 
 using Combinatorics
 using Memoize
+using Primes
 using ..PartitionsGen
 using .EulerCoefficients
+using .TotallyLess
 
 # BRUTE FORCE ALGORITHM
 
@@ -425,5 +428,80 @@ end
 
 partition_number = partition_number_euler
 export partition_number
+
+#### The following code was copied from RosettaCode.com
+#### https://rosettacode.org/wiki/Proper_divisors#Julia
+####
+#### ... and it was adapted for the purposes of this project.
+####
+#### This code thus uses the GNU Free Documentation Liscense         
+#### https://www.gnu.org/licenses/old-licenses/fdl-1.2.html
+
+function properdivisors(n)
+    0 < n || throw(ArgumentError("number to be factored must be ≥ 0, got $n"))
+    1 < n || return Integer[]
+    !isprime(n) || return [1, n]
+    f = factor(n)
+    d = [1]
+    for (k, v) in f
+        c = [k^i for i in 0:v]
+        d = d*c'
+        d = reshape(d, length(d))
+    end
+    sort!(d)
+    return d[1:end]
+end
+
+#### This ends the code licesned under the GNU Free Documentation Liscense
+
+function σ₁(n)
+  n == 1 && return 1
+  sum(properdivisors(n))
+end
+
+#"""
+#Gets the partition number in using the gfology-style
+#generating function
+#"""
+@memoize function partition_number_gfology(a,b,D,allpositive=false)
+  allpositive && throw(ArgumentException("Ok++ not yet supported"))
+  #println("Starting on p($a,$b)")
+
+  (a,b) == (0,0) && return 1
+  !is_wholly_positive(a,b,D) && return 0
+  (a,b) == (1,0) && return 1
+  b < 0 && return partition_number_gfology(a,-b,D,allpositive)
+  
+  # auxillary functions because I'm too lazy to make a datatype
+  norm((a,b)) = a^2 - D*(b^2)
+  bar((a,b)) = (a,-b)
+  times((a,b),(c,d)) = (a*c + b*d*D, b*c + a*d)
+  gcid((a,b)) = gcd(a,b)
+
+  n = (a,b)
+  whollyLessThanN = TotallyLess.listPoints(a,b,D,true)
+  total = (0,0)
+
+  for m in whollyLessThanN
+    p_nminusm = partition_number_gfology((n .- m)...,D,allpositive)
+    g = gcid(m)
+
+    #a == 4 && println("m = $m")
+    #a == 4 && println("p(n-m) = $p_nminusm")
+    coef = div.(m,g) .* σ₁(g)
+    #a == 4 && println("coef for m = $m: $coef")
+    total = @. total + div(m,g) * σ₁(g) * p_nminusm
+    #a == 4 && println("total = $total")
+
+  end
+
+  #a == 4 && println("total = $total")
+  #intterm = times(bar(n), total)
+  #println("n̄ * total = $intterm")
+  #nom = norm(n)
+  #println("N(n) = $nom")
+  div.(times(bar(n), total), norm(n))[1]
+  #println("p($a,$b) = $res")
+end
 
 end#module
