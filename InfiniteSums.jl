0454f6850e8668307c58035bb4b28623d167289b
jjgarzella <jjgarzella@gmail.com>
2020-07-09 15:43:20 -0600

    Add file for computing the constant

---
+++ InfiniteSums.jl
@@ -0,0 +1,40 @@
+
+# basic functions
+
+function zeta(s,n)
+  
+end
+
+function gamma(s)
+  
+end
+
+function heckezeta(s,n)
+  
+end
+
+# main ingredients 
+
+#more parameters will probably be needed.
+
+function frakS1(n,N)
+  
+end
+
+function frakS2(n,N)
+  
+end
+
+function frakS3(n,N)
+  
+end
+
+function partialSum(f,maxN)
+	sum(f.(1:maxN))
+end
+
+function frakSconstant(N,maxNumTerms=1000)
+  
+end
+
+
